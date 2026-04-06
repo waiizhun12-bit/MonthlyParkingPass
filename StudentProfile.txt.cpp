2db866c588ae670dc2d0f56d0a0f5a70f519f3da
KenChin0419 <Kenchin0419@1utar.my>
2026-04-06 15:28:05 +0800

    edited StudentProfile

--- StudentProfile.txt.cpp
+++ StudentProfile.txt.cpp
@@ -1,59 +1,13 @@
-#include <iostream>
+/*#include <iostream>
+#include <string>
 using namespace std;
 
-void systemMenu();
-void loadStudents();
-void loadApplications();
-void loadPayments();
+class studentMenu {
+    string studentId;
+    string name;
+    string password;
+    string transactionHistory[100];
 
-int main()
- {
-    systemMenu();
-    loadStudents();
-    loadApplications();
-    loadPayments();
+    studentMenu(string id, string n ,string pass)
+}*/
 
-    return 0;
-}
-
-
-void systemMenu()
- {
-    cout << "System is starting..." << endl;
-}
-
-void loadStudents()
- {
-    cout << "\nLoading Students..." << endl;
-
-    char name[50];
-
-    cout << "Enter student name: ";
-    cin >> name;
-
-    cout << "Student Loaded: " << name << endl;
-}
-
-void loadApplications()
- {
-    cout << "\nLoading Applications..." << endl;
-
-    char app[50];
-
-    cout << "Enter application name: ";
-    cin >> app;
-
-    cout << "Application Loaded: " << app << endl;
-}
-
-void loadPayments()
- {
-    cout << "\nLoading Payments..." << endl;
-
-    char payment[50];
-
-    cout << "Enter payment info: ";
-    cin >> payment;
-
-    cout << "Payment Loaded: " << payment << endl;
-}
